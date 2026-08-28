2b4036f4fcf2143857fdb143089d1602e7f06308
unknown <anthonyma24@163.com>
2026-08-28 11:46:56 -0400

    test: require release version 0.2.9

--- tests/deployment/test_release_installer_cli.py
+++ tests/deployment/test_release_installer_cli.py
@@ -114,7 +114,7 @@ def test_release_cli_version_reports_package_version() -> None:
 
 
 def test_next_public_release_version_is_0_2_8() -> None:
-    assert _project_version() == "0.2.8"
+    assert _project_version() == "0.2.9"
 
 
 def test_built_release_wheel_contains_shared_agent_rules(tmp_path: Path) -> None:
